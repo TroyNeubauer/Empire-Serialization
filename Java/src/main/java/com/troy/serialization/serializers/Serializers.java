f8519a9b3ccf6ab8ebad89dcb88285b4dc2ff71a
Troy Neubauer <troyneubauer@gmail.com>
2018-03-17 22:17:43 -0400

    Cleaned up serializers code

--- Java/src/main/java/com/troy/serialization/serializers/Serializers.java
+++ Java/src/main/java/com/troy/serialization/serializers/Serializers.java
@@ -1,44 +1,33 @@
 package com.troy.serialization.serializers;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 import com.troy.serialization.util.SerializationUtils;
 
 public class Serializers {
 
 	private final static HashMap<Class<?>, Serializer<?>> map = new HashMap<Class<?>, Serializer<?>>();
-	private static Serializer<Object> defaultSerializer = getDefaultSerializer();
-	
-	public static <T> Serializer<T> getSerializer(Class<T> type) {
-		if (map.containsKey(type)) {
 
-		} else {
-			Serializer<T> result;
-			if (SerializationUtils.DYNAMIC_CLASS_LOADING_ENABLED) {
-				result = createSerializer(type);
-			} else {
-				result = null;
-			}
-			map.put(type, result);
+	public static <T> Serializer<T> getSerializer(Class<T> type) {
+		Serializer<T> ser = (Serializer<T>) map.get(type);
+		if (ser == null) {
+			ser = createSerializer(type);
+			map.put(type, ser);
 		}
-		return null;
+		return ser;
+	}
+
+	public static <T> boolean setSerializer(Class<T> type, Serializer<T> serializer) {
+		return map.put(Objects.requireNonNull(type), Objects.requireNonNull(serializer)) != null;
 	}
 
 	private static <T> Serializer<T> createSerializer(Class<T> type) {
-		if(!SerializationUtils.DYNAMIC_CLASS_LOADING_ENABLED) {//Use the default one if we can't create a dynamic one
-			return (Serializer<T>) getDefaultSerializer();
-		}//Create a dynamic one
-		
-		return null;//TODO create dynamic serializer creation system
+		if (!SerializationUtils.DYNAMIC_CLASS_LOADING_ENABLED) {// Use the default one if we can't create a dynamic one
+			return new FieldSerializer<T>(type);
+		} // Create a dynamic one
+
+		return null;// TODO create dynamic serializer creation system
 	}
-	
-	public static Serializer<Object> getDefaultSerializer() {
-		return Serializers.defaultSerializer;
-	}
-	
-	public static void setDefaultSerializer(Serializer<Object> defaultSerializer) {
-		Serializers.defaultSerializer = defaultSerializer;
-	}
-	
 
 }
