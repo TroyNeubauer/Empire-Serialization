5311021ae1eddb1b5560dbe6f61befc9191af6f9
Troy Neubauer <troyneubauer@gmail.com>
2018-05-08 05:48:25 -0400

    Updated Abstract Guess API

--- Byte Viewer/src/com/troy/byteviewer/guess/AbstractGuess.java
+++ Byte Viewer/src/com/troy/byteviewer/guess/AbstractGuess.java
@@ -13,17 +13,22 @@ public abstract class AbstractGuess {
 	private static final Color[] ROTATION = { Color.MAGENTA, Color.ORANGE, Color.LIGHT_GRAY, Color.PINK, Color.RED };
 	private static int colorIndex;
 
-	public void read(Input input) {
-		maxOffset += readImpl(input, guesses);
+	public void read(Input input, long minimunIndex) {
+		long toRead = maxOffset - minimunIndex;
+		if (toRead <= 0)
+			return;
+		maxOffset += readImpl(input, toRead, guesses);
 	}
 
 	/**
-	 * Reads for guesses
-	 * @param input
-	 * @param guesses
-	 * @return
+	 * Reads for guesses and adds them to the list
+	 * 
+	 * @param input The input to read from
+	 * @param minBytes The minimum number of bytes to read
+	 * @param guesses The list of guesses to add to
+	 * @return The number of actual bytes read (must be > {@code minBytes})
 	 */
-	public abstract long readImpl(Input input, List<AnnotatedSection> guesses);
+	public abstract long readImpl(Input input, long minBytes, List<AnnotatedSection> guesses);
 
 	protected Color nextColor() {
 		colorIndex %= ROTATION.length;
