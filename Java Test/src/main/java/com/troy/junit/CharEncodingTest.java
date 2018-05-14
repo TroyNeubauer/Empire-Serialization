6054af1394cbcc8b6254c47c7070b40275d3f0d9
Troy Neubauer <troyneubauer@gmail.com>
2018-05-14 11:57:07 -0400

    Created string encoding test

--- Java Test/src/main/java/com/troy/junit/CharEncodingTest.java
+++ Java Test/src/main/java/com/troy/junit/CharEncodingTest.java
@@ -11,16 +11,15 @@ import com.troy.empireserialization.io.out.*;
 import com.troy.empireserialization.util.*;
 
 public class CharEncodingTest {
-	
-	private static final char[] FOUR_BIT_TEST = "test".toCharArray();
+
+	private static final char[] FOUR_BIT_TEST = TestConstants.FOUR_BIT_STRING.toCharArray();
 	private static final byte[] FOUR_BIT_RESULT = new byte[] { 0b00010000, 0b01100001 };
 
-	private static final char[] SIX_BIT_TEST = "This is Empire Serialization written by Troy Neubauer!".toCharArray();
-	private static final byte[] SIX_BIT_RESULT = new byte[] { -27, 53, 30, 1, 71, -128, -87, -122, -44, 117, 0, 56, 65,
-			-43, 12, 93, 73, 76, 125, 70, -103, 2, 39, 84, 125, -12, 25, 0, -39, 0, -27, -42, -92, 3, 52, 32, 52, -56,
-			16, 7, 66 };
-	
-	private static final char[] VLE8_TEST = ("This is a test VLE8 String next char is scarry:" +'\u1969').toCharArray();
+	protected static final char[] SIX_BIT_TEST = TestConstants.SIX_BIT_STRING.toCharArray();
+	private static final byte[] SIX_BIT_RESULT = new byte[] { -27, 53, 30, 1, 71, -128, -87, -122, -44, 117, 0, 56, 65, -43, 12, 93, 73, 76, 125, 70,
+			-103, 2, 39, 84, 125, -12, 25, 0, -39, 0, -27, -42, -92, 3, 52, 32, 52, -56, 16, 7, 66 };
+
+	private static final char[] VLE8_TEST = TestConstants.VLE8_STRING.toCharArray();
 
 	@Test
 	public void nativeFourBit() {
@@ -35,8 +34,7 @@ public class CharEncodingTest {
 		NativeUtils.NATIVES_ENABLED = false;
 		ByteArrayOutput out = new ByteArrayOutput();
 		EmpireCharsets.FOUR_BIT_CHARSET.encode(FOUR_BIT_TEST, out, 0, FOUR_BIT_TEST.length, StringInfo.ALL_ASCII);
-		assertArrayEquals(Arrays.copyOf(out.getBuffer(), out.getBufferPosition()),
-				new byte[] { 0b00010000, 0b01100001 });
+		assertArrayEquals(Arrays.copyOf(out.getBuffer(), out.getBufferPosition()), new byte[] { 0b00010000, 0b01100001 });
 	}
 
 	@Test
@@ -48,7 +46,7 @@ public class CharEncodingTest {
 
 		assertArrayEquals(Arrays.copyOf(out.getBuffer(), out.getBufferPosition()), SIX_BIT_RESULT);
 	}
-	
+
 	@Test
 	public void javaSixBit() {
 		NativeUtils.NATIVES_ENABLED = false;
