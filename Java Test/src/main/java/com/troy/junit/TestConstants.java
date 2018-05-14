f1fae8026e2554da01a76c5b875075ac903bf12f
Troy Neubauer <troyneubauer@gmail.com>
2018-05-14 11:57:41 -0400

    Added TestConstants to keep constants consistant between test classes

---
+++ Java Test/src/main/java/com/troy/junit/TestConstants.java
@@ -0,0 +1,7 @@
+package com.troy.junit;
+
+public class TestConstants {
+	public static final String FOUR_BIT_STRING = "test";
+	public static final String SIX_BIT_STRING = "This is Empire Serialization written by Troy Neubauer!";
+	public static final String VLE8_STRING = "This is a test VLE8 String. next char is scarry:" + '\u1969';
+}
