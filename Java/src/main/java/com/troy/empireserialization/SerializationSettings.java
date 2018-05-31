c66044853398abe4c702b60349b1380f0f1c69d5
Troy Neubauer <troyneubauer@gmail.com>
2018-05-30 22:37:21 -0400

    Changed settings (still more work to do)

--- Java/src/main/java/com/troy/empireserialization/SerializationSettings.java
+++ Java/src/main/java/com/troy/empireserialization/SerializationSettings.java
@@ -2,28 +2,54 @@ package com.troy.empireserialization;
 
 public class SerializationSettings {
 	public static final SerializationSettings defaultSettings = new SerializationSettings();
-	
-	static {
-		defaultSettings.inferFinalClasses = true;
-	}
+
 
 	public boolean inferFinalClasses;
+	public boolean useObjectCache, useStringCache;
+
 	public boolean useVLE;
 	public boolean useLittleEndian;
-	public int version = Version.VERSION;
+	public boolean pluginsPresent;
+	public int version = Version.VERSION_1;
 
 	public SerializationSettings() {
-
+		inferFinalClasses = true;
+		useVLE = true;
+		useObjectCache = true;
+		useStringCache = true;
 	}
 
-	
+	public SerializationSettings inferFinalClasses(boolean inferFinalClasses) {
+		this.inferFinalClasses = inferFinalClasses;
+		return this;
+	}
+
+	public SerializationSettings useObjectCache(boolean useObjectCache) {
+		this.useObjectCache = useObjectCache;
+		return this;
+	}
+
+	public SerializationSettings useStringCache(boolean useStringCache) {
+		this.useStringCache = useStringCache;
+		return this;
+	}
+
+	public SerializationSettings useVLE(boolean useVLE) {
+		this.useVLE = useVLE;
+		return this;
+	}
+
+	public SerializationSettings useLittleEndian(boolean useLittleEndian) {
+		this.useLittleEndian = useLittleEndian;
+		return this;
+	}
+
 	public static SerializationSettings getDefaultSettings() {
 		return defaultSettings;
 	}
 
+	public int getFlagsData() {
 
-	public int getFlagsData(boolean pluginsPresent) {
-		
 		return 0;
 	}
 
